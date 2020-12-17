6b3ac8271bb1c13331cc743c00226c3b9c7b0190
Yuhara <so960397@icloud.com>
2020-12-17 20:25:55 +0900

    Initial Commit

---
+++ Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dictionary
+//
+//  Created by 湯原壮一朗 on 2020/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
